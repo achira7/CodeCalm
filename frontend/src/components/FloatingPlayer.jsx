99c459c0b3fb2c88f518becaf3cfc6513257955e
Semora <senalweerasekara@gmail.com>
2024-07-07 03:03:58 +0530

    music player changes

--- frontend/src/components/FloatingPlayer.jsx
+++ frontend/src/components/FloatingPlayer.jsx
@@ -6,7 +6,9 @@ import React, { useState, useRef, useEffect } from "react";
 import { Color } from "../theme/Colors"; // Assuming Color.outSideCard provides necessary styling for outer container
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import noIcon from '../../assets/player/music.png'
+
+import noIcon from "../../assets/player/music.png";
+import { useExtractColor } from "react-extract-colors";
 
 const FloatingPlayer = () => {
   const [player, setPlayer] = useRecoilState(playerState);
@@ -16,13 +18,17 @@ const FloatingPlayer = () => {
   const [userID, setUserID] = useState("");
   const audioRef = useRef(null);
   const imageRef = useRef(null);
-  const [dominantColor, setDominantColor] = useState("#000");
+  //   const [dominantColor, setDominantColor] = useState("#b0cf00");
   const [progress, setProgress] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
-  
+
   const [duration, setDuration] = useState(0);
-  const normalMode = `mt-10 p-8 lg:w-1/3 rounded-xl ${Color.playerBG}`
-  const floatMode = 'fixed bottom-2 left-2 border border-gray-300 border-3 bg-opacity-60 backdrop-blur-sm '
+  const normalMode = `mt-10 p-8 lg:w-1/3 rounded-xl ${Color.playerBG}`;
+  const floatMode =
+    "fixed bottom-2 left-2 border border-gray-300 border-3 bg-opacity-60 backdrop-blur-sm ";
+  const { colors, dominantColor, darkerColor, lighterColor, loading, error } =
+    useExtractColor(currentTrack.image);
+  const gradient = `linear-gradient(135deg, ${dominantColor} 40%, ${lighterColor} 60%)`;
 
   const handleNext = () => {
     setPlayer((prev) => ({
@@ -99,113 +105,129 @@ const FloatingPlayer = () => {
   }, [player.currentTrackIndex, userID]);
 
   return (
-    <div className={`${player.isFloating ? "" : window.location.pathname.includes('/employee/player') ? `flex justify-center   ${Color.background}` : '' }`}>
-
-    
     <div
-      //   className={`fixed bottom-2 left-2 ${
-      className={` bg-opacity-30 ${
-        Color.playerBG
-      } rounded-lg p-4 ${
-        player.isFloating ? floatMode : window.location.pathname.includes('/employee/player') ? normalMode : 'hidden'
+      className={`${
+        player.isFloating
+          ? ""
+          : window.location.pathname.includes("/employee/player")
+          ? `flex justify-center   ${Color.background}`
+          : ""
       }`}
     >
-      <div className="flex justify-between">
-        <div className="mb-8 flex items-center">
-          <div className="mr-3">
-            <img
-              src={currentTrack.image || noIcon}
-              alt={currentTrack.title}
-              className="rounded-lg w-16 h-16 object-cover border-2 border-gray-200"
-            />
+      <div
+        //   className={`fixed bottom-2 left-2 ${
+        className={`bg-opacity-30 ${Color.playerBG} rounded-lg p-4 ${
+          player.isFloating
+            ? floatMode
+            : window.location.pathname.includes("/employee/player")
+            ? normalMode
+            : "hidden"
+        }`}
+        style={{ background: gradient }}
+      >
+        <div className="flex justify-between">
+          <div className="mb-8 flex items-center">
+            <div className="mr-3">
+              <img
+                src={currentTrack.image || noIcon}
+                alt={currentTrack.title}
+                className="rounded-lg w-16 h-16 object-cover border-2 border-gray-200"
+              />
+            </div>
+            <div>
+              <div className="font-bold text-lg">{currentTrack.title}</div>
+              <div className="text-md">{currentTrack.artist}</div>
+            </div>
           </div>
           <div>
-            <div className="font-bold text-lg">{currentTrack.title}</div>
-            <div className="text-md">{currentTrack.artist}</div>
+            {window.location.pathname.includes("/employee/player") ? (
+              ""
+            ) : (
+              <button
+                className="text-white hover:text-sky-500"
+                onClick={() =>
+                  setPlayer((prev) => ({ ...prev, isFloating: false }))
+                }
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            )}
           </div>
         </div>
-        <div>
-        {window.location.pathname.includes('/employee/player') ? "" : 
-          <button
-            className="text-white hover:text-sky-500"
-            onClick={() =>
-              setPlayer((prev) => ({ ...prev, isFloating: false }))
-            }
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+
+        {window.location.pathname.includes("/employee/player") ? (
+          <div className="relative mb-8 flex justify-center z-10">
+            <div
+              className={` circle-content ${
+                player.isPlaying ? "breathing" : ""
+              } circle-border`}
+              style={{ "--dominant-color": dominantColor }}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-         }
-        </div>
-      </div>
-
-      {window.location.pathname.includes('/employee/player') ? 
-      <div className="relative mb-8 flex justify-center z-10">
-        <div
-          className={`circle-content ${
-            player.isPlaying ? "breathing" : ""
-          } circle-border`}
-          style={{ "--dominant-color": dominantColor }}
-        >
-          <div className="relative" style={{ width: "250px", height: "250px" }}>
-            <CircularProgressbar
-              strokeWidth={2}
-              value={progress}
-              size={10}
-              styles={buildStyles({
-                pathColor: "rgb(0, 161, 219)",
-                trailColor: "#d6d6d6",
-                strokeLinecap: "butt",
-                pathTransitionDuration: 0.5,
-              })}
-            />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <img
-                ref={imageRef}
-                src={currentTrack.image}
-                alt={currentTrack.title}
-                className={`w-56 h-56 rounded-full ${
-                  player.isPlaying ? "spinning" : "paused"
-                }`}
-              />
+              <div
+                className="relative"
+                style={{ width: "250px", height: "250px" }}
+              >
+                <CircularProgressbar
+                  strokeWidth={2}
+                  value={progress}
+                  size={10}
+                  styles={buildStyles({
+                    pathColor: "rgb(0, 161, 219)",
+                    trailColor: "#d6d6d6",
+                    strokeLinecap: "butt",
+                    pathTransitionDuration: 0.5,
+                  })}
+                />
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <img
+                    ref={imageRef}
+                    src={currentTrack.image}
+                    alt={currentTrack.title}
+                    className={`w-56 h-56 rounded-full ${
+                      player.isPlaying ? "spinning" : "paused"
+                    }`}
+                  />
+                </div>
+              </div>
+              <h2 className="text-white text-center song-title">
+                <div className="bg-gray-800/90 rounded-full w-32 h-32 flex justify-center items-center">
+                  {formatTime(currentTime)}/{formatTime(duration)}
+                </div>
+              </h2>
             </div>
           </div>
-          <h2 className="text-white text-center song-title">
-            <div className="bg-gray-800/90 rounded-full w-32 h-32 flex justify-center items-center">
-              {formatTime(currentTime)}/{formatTime(duration)}
-            </div>
-          </h2>
-        </div>
-      </div>
-      : ""}
+        ) : (
+          ""
+        )}
 
-      <AudioPlayer
-        autoPlay={false}
-        ref={audioRef}
-        src={currentTrack.audioSrc}
-        onPlay={handlePlayPause}
-        onPause={handlePlayPause}
-        onEnded={handleNext}
-        showJumpControls={true}
-        showSkipControls={true}
-        onClickNext={handleNext}
-        onClickPrevious={handlePrevious}
-        loop={player.isLoop}
-        className="custom-audio-player"
-      />
-    </div>
+        <AudioPlayer
+          autoPlay={false}
+          ref={audioRef}
+          src={currentTrack.audioSrc}
+          onPlay={handlePlayPause}
+          onPause={handlePlayPause}
+          onEnded={handleNext}
+          showJumpControls={true}
+          showSkipControls={true}
+          onClickNext={handleNext}
+          onClickPrevious={handlePrevious}
+          loop={player.isLoop}
+          style={{ backgroundColor : currentTrack.color }}
+        />
+      </div>
     </div>
   );
 };
--- frontend/src/components/Player.jsx
+++ frontend/src/components/Player.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
-import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import "tailwindcss/tailwind.css";
 import "./PlayerStyles.css";
@@ -9,7 +8,11 @@ import "react-circular-progressbar/dist/styles.css";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { playerState, tracksState } from "../atoms";
 import song from '../../assets/temp/song.mp3';
+import son2 from '../../assets/temp/Funhouse.mp3';
+import son3 from '../../assets/temp/sample.mp3';
 import { Color } from "../theme/Colors";
+
+
 
 const Player = () => {
   const [player, setPlayer] = useRecoilState(playerState);
@@ -36,22 +39,22 @@ const Player = () => {
         title: "White Noise",
         artist: "CodeCalm",
         audioSrc: song,
-        image: 'https://plus.unsplash.com/premium_photo-1689974465650-b223928cdc9e?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        color: "#00aeb0"
+        image: 'https://images.unsplash.com/photo-1542051841857-5f90071e7989?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        color: "#275174"
       },
       {
         title: "Rain",
         artist: "CodeCalm",
-        audioSrc: 'http://127.0.0.1:8000/media/audio/rain.mp3',
-        image: '',
-        color: "#ffb77a"
+        audioSrc: son2,
+        image: 'https://images.unsplash.com/photo-1524413840807-0c3cb6fa808d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        color: "#8d5979"
       },
       {
         title: "Sea",
         artist: "CodeCalm",
-        audioSrc: 'http://127.0.0.1:8000/media/audio/sea.mp3',
-        image: '',
-        color: "#5f9fff"
+        audioSrc: son3,
+        image: 'https://images.unsplash.com/photo-1606542758304-820b04394ac2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        color: "#5529267d"
       }
     ]);
   }, []);
@@ -79,7 +82,8 @@ const Player = () => {
       >
         {player.isFloating ? "Switch to Normal Mode" : "Switch to Floating Mode"}
       </button>
-      <div className="h-screen">
+      <div className="h-screen overflow-y-hidden">
+
       </div>
     </div>
   );
